f40da5f10baa1b0881faa2beaa5081858b107a8c
Mirel Springer <mirel.springer@benmanage.com>
2019-12-16 15:48:42 +0200

    Create basic layout and include packages

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const electron = require('electron');
 const { app, BrowserWindow } = require('electron');
 
+const letterCombosULine = [ 'qu', 'ck,' 'ff', 'ss', 'll,' 'zz',
+'ch', 'ar', 'sh', 'or', 'th', 'ur', 'ng', 'ow', 'ai', 'oi', 'ee',
+'ear', 'igh', 'air', 'oa', 'ure', 'oo', 'er', 'oy', 'ou', 'ay' ];
+
+const wordsRed = [ 'the', 'to', 'I', 'go', 'no', 'in', 'he', 'she',
+ 'we', 'me', 'be', 'was', 'my', 'you', 'her', 'they', 'all', 'are',
+  'said', 'so', 'do', 'have', 'like', 'some', 'come', 'were',
+	 'there', 'little', 'one', 'when', 'out', 'what', 'Peter',
+	  'Jane', 'water', 'here' ];
+
+const lettersUDot = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+ 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u',
+  'v', 'w', 'x', 'y', 'z' ];
+
 function createWindow() {
 	let win = new BrowserWindow({
 		width: 800,
@@ -11,6 +25,16 @@ function createWindow() {
 	});
 	win.show();
 	win.loadFile('index.html');
+
+	win.on('closed', () => {
+		win = null;
+	});
 }
 
 app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
