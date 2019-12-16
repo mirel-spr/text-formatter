c4c9d07ce673ee5f432428f54f5bb2317704a759
Mirel Springer <44804291+mirel-spr@users.noreply.github.com>
2019-12-16 13:35:36 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const electron = require('electron');
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+	let win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+	win.show();
+	win.loadFile('index.html');
+}
+
+app.on('ready', createWindow);
